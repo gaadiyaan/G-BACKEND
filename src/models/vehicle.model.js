8b101360f8c3e5e0411cdcd9d5fa494efc9d0e95
Jerryy <139037189+RohitSingh-18@users.noreply.github.com>
2025-01-21 04:48:24 +0530

    Update vehicle.model.js

--- src/models/vehicle.model.js
+++ src/models/vehicle.model.js
@@ -22,6 +22,7 @@ const createTableQuery = `
     images JSON NULL DEFAULT NULL,
     specifications JSON NULL DEFAULT NULL,
     features JSON NULL DEFAULT NULL,
+    created_by_email VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
   )`;
